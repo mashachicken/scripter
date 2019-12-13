9c3c8ad611e34c220d25ffde70b30c42fe7bcfde
Student <example@example.com>
2019-12-12 16:32:49 -0800

    adds functionality to place projoct in new folder

--- scripter.rb
+++ scripter.rb
@@ -1,8 +1,12 @@
 require 'fileutils'
 
-FileUtils.mkdir './lib'
-FileUtils.mkdir './spec'
-FileUtils.touch './GemFile'
+# whats the name of your project
+puts "Name of your project"
+project_name = gets.chomp.downcase
+
+FileUtils.mkdir "./#{project_name}"
+FileUtils.mkdir "./#{project_name}/lib"
+FileUtils.mkdir "./#{project_name}/spec"
 class_names = []
 
 gemfile_contents = "
@@ -12,10 +16,11 @@ gem 'rspec'
 gem 'pry'
 "
 
-IO.write('./GemFile', gemfile_contents)
+FileUtils.touch "./#{project_name}/GemFile"
+File.write("./#{project_name}/GemFile", gemfile_contents)
 
 got_classes = false
-
+# Get User Classes
 while got_classes == false
   puts "Name a class or type 'done' to move on: "
   temp = gets.chomp
@@ -37,6 +42,28 @@ require '#{name.downcase}.rb'
 describe('#{name.capitalize}') do
 end
 "
-IO.write("./lib/#{name.downcase}.rb", generic_class_content)
-IO.write("./spec/#{name.downcase}_spec.rb", generic_spec_content)
+File.write("./#{project_name}/lib/#{name.downcase}.rb", generic_class_content)
+File.write("./#{project_name}/spec/#{name.downcase}_spec.rb", generic_spec_content)
 end
+
+# Need to figure out : --------------------
+
+# Bundle Install
+
+
+# # Get Attributes : -----------------------
+# get_attr = false
+# attr_names = []
+#
+# while get_attr == false
+#   puts "Attribute Names? type 'done' to move on"
+#   temp_attr_name = gets.chomp
+#   if temp_attr_name.downcase == 'done'
+#     get_attr = true
+#
+#     attr_names = []
+#   else
+#     attr_names.push(temp_attr_name)
+#   end
+#   class_names.push(temp)
+# end
